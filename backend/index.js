5f1810d8692b12217cfb983463c56f0aaeae96e8
Alexanndre85DF <tolentinoalexandre534@gmail.com>
2025-05-22 23:14:39 -0300

    Corrige conflito de porta com Render

--- backend/index.js
+++ backend/index.js
@@ -59,7 +59,7 @@ app.delete('/assessoramentos/:id', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 }); 
